49e9572dd0beedfd808119c2f895f774bc09c885
davay <davay@devinl.im>
2021-08-27 09:55:45 -0700

    feat: easy setup

---
+++ setup.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Dependencies
+
+packages=("jq" "moreutils" "python3")
+
+for pkg in ${packages[@]}; do
+
+	is_pkg_installed=$(dpkg-query -W --showformat='${Status}\n' ${pkg} | grep "install ok installed")
+	if [ "${is_pkg_installed}" == "install ok installed" ]; then
+		echo "${pkg} is installed."; else
+		sudo apt install -yq ${pkg}
+	fi
+done
+
+pip3 install -r requirements.txt
+
+# Ask for user Discord Bot Token
+
+discord_token=""
+current_dir=$(pwd)
+
+printf "\n"
+echo "==============================================================================="
+echo "Please input Discord Bot Token, see https://discord.com/developers/applications"
+echo "==============================================================================="
+printf "\n"
+
+read -p "Token: " discord_token
+
+# Insert token to src/resources/config.json
+
+echo $(jq --arg token "$discord_token" '.DISCORD_TOKEN = $token' $current_dir/src/resources/config.json) | sponge $current_dir/src/resources/config.json
+
+# Create Service File
+
+echo "[Unit]
+Description=Mokujin Tekken Discord Bot
+
+[Service]
+Type=simple
+User=$USER
+ExecStart=/usr/bin/python3 $current_dir/src/mokujin.py
+
+[Install]
+WantedBy=multi-user.target" | sudo dd of=/etc/systemd/system/mokujin.service status=none
+
+sudo systemctl enable mokujin.service
+sudo systemctl start mokujin.service
